ca35bf50cac3c5398c38af0fedcf9b33cafe129f
Ellen Shapiro <designatednerd@gmail.com>
2016-06-29 21:57:48 -0500

    Basic color stuff based on discussions with design

--- SpotHero_iOS_Partner_SDK/Classes/SpotHeroPartnerSDK.swift
+++ SpotHero_iOS_Partner_SDK/Classes/SpotHeroPartnerSDK.swift
@@ -17,6 +17,11 @@ public class SpotHeroPartnerSDK: NSObject {
     /// Set to `true` if you would like a very large amount of debug logging. DO NOT set this to `true` for release builds. 
     public var debugPrintInfo = false
     
+    /// The tint color to use for the background of the nav bar.
+    public var tintColor: UIColor = .shp_spotHeroBlue()
+    
+    /// The text color to use for the nav bar. 
+    public var textColor: UIColor = .whiteColor()
     
     //MARK: Error keys
     
