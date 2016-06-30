7dc9b0e3b126ed58a5c4a3750357d7f7d3a97051
Ellen Shapiro <designatednerd@gmail.com>
2016-06-29 19:23:08 -0500

    Fix copyright

--- Example/SpotHero_iOS_Partner_SDK/ViewController.swift
+++ Example/SpotHero_iOS_Partner_SDK/ViewController.swift
@@ -3,7 +3,7 @@
 //  SpotHero_iOS_Partner_SDK
 //
 //  Created by Ellen Shapiro on 06/29/2016.
-//  Copyright (c) 2016 Ellen Shapiro. All rights reserved.
+//  Copyright (c) 2016 SpotHero, Inc. All rights reserved.
 //
 
 import UIKit
