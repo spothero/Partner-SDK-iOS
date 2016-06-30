b421ee1762cc84b96a407e03585795e495fa2347
Ellen Shapiro <designatednerd@gmail.com>
2016-06-29 19:22:53 -0500

    Add UI tests, rename things better

---
+++ Example/SpotHero_iOS_Partner_SDK_ExampleUITests/ShowSDKUITests.swift
@@ -0,0 +1,16 @@
+//
+//  ShowSDKUITests.swift
+//  SpotHero_iOS_Partner_SDK_ExampleUITests
+//
+//  Created by Ellen Shapiro (Work) on 6/29/16.
+//  Copyright © 2016 SpotHero, Inc. All rights reserved.
+//
+
+import XCTest
+
+class ShowSDKUITests: XCTestCase {
+
+    func testExample() {
+        XCTFail("Tests don't actually exist yet")
+    }
+}
