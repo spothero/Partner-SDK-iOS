6b5104dff0b5b88b8c44046c4b694c54e5c1b068
Ellen Shapiro <designatednerd@gmail.com>
2016-06-29 20:10:32 -0500

    Add kif helpers

---
+++ Example/SpotHero_iOS_Partner_SDK_ExampleUITests/XCTestCase+KIF.swift
@@ -0,0 +1,30 @@
+//
+//  XCTestCase+KIF.swift
+//  SpotHero_iOS_Partner_SDK
+//
+//  Created by Ellen Shapiro (Work) on 6/29/16.
+//  Copyright © 2016 SpotHero, Inc. All rights reserved.
+//
+
+/*
+ Helpers to allow KIF to be accessed from Swift.
+ */
+extension XCTestCase {
+    func tester(file: String = #file, _ line: Int = #line) -> KIFUITestActor {
+        return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+    }
+    
+    func system(file: String = #file, _ line: Int = #line) -> KIFSystemTestActor {
+        return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+    }
+}
+
+extension KIFTestActor {
+    func tester(file: String = #file, _ line: Int = #line) -> KIFUITestActor {
+        return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+    }
+    
+    func system(file: String = #file, _ line: Int = #line) -> KIFSystemTestActor {
+        return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+    }
+}
