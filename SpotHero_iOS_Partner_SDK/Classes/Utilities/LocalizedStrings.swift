24838ecba0d5d5c25eb9907cd08ee336d00a0bc6
Husein Kareem <huseinkareem@gmail.com>
2016-07-13 15:15:04 -0500

    add localized and accessibility strings and constants

---
+++ SpotHero_iOS_Partner_SDK/Classes/Utilities/LocalizedStrings.swift
@@ -0,0 +1,18 @@
+//
+//  LocalizedStrings.swift
+//  Pods
+//
+//  Created by Husein Kareem on 7/13/16.
+//
+//
+
+import Foundation
+
+enum LocalizedStrings {
+    static let close = NSLocalizedString("Close",
+                                         comment: "Close bar button title")
+    static let launchSDK = NSLocalizedString("Launch SDK",
+                                             comment: "Launch SDK button title")
+    static let findParking = NSLocalizedString("Find Parking",
+                                               comment: "Find parking view title")
+}
