6b27cfb81c0e706abd972f48eb9b243d4153fa8e
Matt Reed <matt.reed@spothero.com>
2016-07-22 13:08:41 -0500

    Add Reservation and reservation api structs

---
+++ SpotHero_iOS_Partner_SDK/Classes/Models/Reservation.swift
@@ -0,0 +1,13 @@
+//
+//  Reservation.swift
+//  Pods
+//
+//  Created by SpotHeroMatt on 7/22/16.
+//
+//
+
+import Foundation
+
+struct Reservation {
+    
+}
---
+++ SpotHero_iOS_Partner_SDK/Classes/ReservationAPI.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationAPI.swift
+//  Pods
+//
+//  Created by SpotHeroMatt on 7/22/16.
+//
+//
+
+import Foundation
+
+struct ReservationAPI {
+    func createReservation(facility: Facility, rate: Rate, completion: (Reservation?) -> (Void))  {
+        let params = [
+            
+        ]
+    }
+}
