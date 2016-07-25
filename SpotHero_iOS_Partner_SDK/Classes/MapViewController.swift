8b949f3b7a1b8b783f01e5ae871352e2415a9d94
Husein Kareem <huseinkareem@gmail.com>
2016-07-25 18:18:25 -0500

    add and setup date picker view

--- SpotHero_iOS_Partner_SDK/Classes/MapViewController.swift
+++ SpotHero_iOS_Partner_SDK/Classes/MapViewController.swift
@@ -21,9 +21,21 @@ class MapViewController: UIViewController {
     @IBOutlet weak var reservationContainerView: UIView!
     @IBOutlet weak var reservationContainerViewHeightConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var endTimeLabel: UILabel!
+    @IBOutlet weak var endDateLabel: UILabel!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var datePickerViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var datePickerView: UIView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var toolbar: UIToolbar!
+    
     let predictionController = PredictionController()
     private let searchBarHeight: CGFloat = 44
     private let reservationContainerViewHeight: CGFloat = 134
+    private var isStartView = false
+    private var startDate: NSDate?
+    private var endDate: NSDate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +54,9 @@ class MapViewController: UIViewController {
         self.reservationContainerView.layer.cornerRadius = 5
         self.reservationContainerView.layer.masksToBounds = true
         
+        self.setupTimeSelectionView()
+        self.setupDatePickerView()
+
         self.predictionController.delegate = self
         
         self.predictionTableView.dataSource = self.predictionController
@@ -60,8 +75,28 @@ class MapViewController: UIViewController {
     }
     
     private func hideTimeSelectionView() {
+        self.datePickerView.hidden = true
         self.timeSelectionView.hidden = true
         self.reservationContainerViewHeightConstraint.constant = self.searchBarHeight
+    }
+    
+    private func setupTimeSelectionView() {
+        //TODO: Localize
+        //TODO: refactor to use UIColor extension
+        self.endDateLabel.text = "Select Time"
+        self.endTimeLabel.text = ""
+        self.endDateLabel.textColor = UIColor(red: 41.0/255.0,
+                                              green: 91.0/255.0,
+                                              blue: 106.0/255.0,
+                                              alpha: 1.0)
+        self.startDateLabel.textColor = UIColor(red: 0/255.0,
+                                                green: 122/255.0,
+                                                blue: 255/255.0,
+                                                alpha: 1.0)
+        self.startTimeLabel.textColor = UIColor(red: 0/255.0,
+                                                green: 122/255.0,
+                                                blue: 255/255.0,
+                                                alpha: 1.0)
     }
     
     private func showTimeSelectionView() {
@@ -79,11 +114,55 @@ class MapViewController: UIViewController {
     }
     
     @IBAction func startViewTapped(sender: AnyObject) {
-        //TODO: Show date picker
+        self.isStartView = true
+        self.showDatePickerView()
     }
     
     @IBAction func endViewTapped(sender: AnyObject) {
-        //TODO: Show date picker
+        self.isStartView = false
+        self.showDatePickerView()
+    }
+    
+    private func setupDatePickerView() {
+        self.hideDatePickerView()
+        self.datePicker.minimumDate = self.dateByRoundingMinutesDownBy30()
+        self.datePicker.date = self.dateByRoundingMinutesDownBy30()
+    }
+    
+    private func showDatePickerView() {
+        UIView.animateWithDuration(0.3) { 
+            self.datePickerViewBottomConstraint.constant = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func hideDatePickerView() {
+        UIView.animateWithDuration(0.3) { 
+            self.datePickerViewBottomConstraint.constant = -200
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func doneButtonPressed(sender: AnyObject) {
+        self.hideDatePickerView()
+    }
+    
+    @IBAction func datePickerValueDidChange(datePicker: UIDatePicker) {
+        let date = datePicker.date
+        if (self.isStartView) {
+            
+        } else {
+            
+        }
+    }
+    
+    private func dateByRoundingMinutesDownBy30() -> NSDate {
+        let today = NSDate()
+        let unitFlags: NSCalendarUnit = [.Minute, .Second]
+        let timeComponents = NSCalendar.currentCalendar().components(unitFlags, fromDate: today)
+        let remain = timeComponents.minute % 30
+        let interval: NSTimeInterval = Double(-((60 * remain) + timeComponents.second))
+        return today.dateByAddingTimeInterval(interval)
     }
 }
 
