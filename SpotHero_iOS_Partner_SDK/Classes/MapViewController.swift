3e4917a43145a383017ffb71123c719ed70e98d1
Husein Kareem <huseinkareem@gmail.com>
2016-07-27 10:46:32 -0500

    setup collapsed view and date picker logic

--- SpotHero_iOS_Partner_SDK/Classes/MapViewController.swift
+++ SpotHero_iOS_Partner_SDK/Classes/MapViewController.swift
@@ -38,6 +38,7 @@ class MapViewController: UIViewController {
     private var isStartView = false
     private var startDate: NSDate?
     private var endDate: NSDate?
+    private var datePickerDate: NSDate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -76,92 +77,183 @@ class MapViewController: UIViewController {
         self.predictionTableView.accessibilityLabel = AccessibilityStrings.PredictionTableView
     }
     
-    private func hideTimeSelectionView() {
-        self.datePickerView.hidden = true
-        self.timeSelectionView.hidden = true
-        self.reservationContainerViewHeightConstraint.constant = self.searchBarHeight
-    }
-    
-    private func setupTimeSelectionView() {
-        //TODO: Localize
-        //TODO: refactor to use UIColor extension
-        self.endDateLabel.text = "Select Time"
-        self.endTimeLabel.text = ""
-        self.endDateLabel.textColor = UIColor(red: 41.0/255.0,
-                                              green: 91.0/255.0,
-                                              blue: 106.0/255.0,
-                                              alpha: 1.0)
-        self.startDateLabel.textColor = UIColor(red: 0/255.0,
-                                                green: 122/255.0,
-                                                blue: 255/255.0,
-                                                alpha: 1.0)
-        self.startTimeLabel.textColor = UIColor(red: 0/255.0,
-                                                green: 122/255.0,
-                                                blue: 255/255.0,
-                                                alpha: 1.0)
-    }
-    
-    private func showTimeSelectionView() {
-        self.timeSelectionView.hidden = false
-        self.reservationContainerViewHeightConstraint.constant = self.reservationContainerViewHeight
-    }
-    
     @IBAction private func closeButtonPressed(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func collapsedSearchBarTapped(sender: AnyObject) {
         self.collapsedSearchBar.hide()
+        self.showTimeSelectionView()
+    }
+    
+    private func showCollapsedSearchBar() {
+        if (self.startDate != nil && self.endDate != nil && self.searchBar.text?.characters.count > 0) {
+            self.collapsedSearchBar.show()
+            self.hideTimeSelectionView()
+            //TODO: Set time label
+        }
+    }
+    
+    //MARK: Time Selection View - setup like collapsed search bar view?
+    
+    private func setupTimeSelectionView() {
+        let startDate = self.dateByRoundingMinutesDownBy30()
+        let endDate = self.dateByRoundingMinutesUpBy30(startDate)
+        self.startDate = startDate
+        self.endDate = endDate
+        self.startTimeLabel.text = DateFormatter.TimeOnly.stringFromDate(startDate)
+        self.startDateLabel.text = DateFormatter.PrettyMonthDayDate.stringFromDate(startDate)
+        self.endTimeLabel.text = DateFormatter.TimeOnly.stringFromDate(endDate)
+        self.endDateLabel.text = DateFormatter.PrettyMonthDayDate.stringFromDate(endDate)
+        
+    }
+    
+    private func showTimeSelectionView() {
+        self.timeSelectionView.hidden = false
+        UIView.animateWithDuration(0.3) {
+            self.reservationContainerViewHeightConstraint.constant = self.reservationContainerViewHeight
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func hideTimeSelectionView() {
+        self.hideDatePickerView()
         self.timeSelectionView.hidden = true
+        UIView.animateWithDuration(0.3) {
+            self.reservationContainerViewHeightConstraint.constant = self.searchBarHeight
+            self.view.layoutIfNeeded()
+        }
     }
     
     @IBAction func startViewTapped(sender: AnyObject) {
         self.isStartView = true
+        self.setStartEndViewSelected()
         self.showDatePickerView()
+        self.datePicker.minimumDate = self.startDate
+        //FIXME: use datebyroundingdown enddate
+        self.datePicker.maximumDate = self.endDate
+        //pass in so no guard let everywhere?
+        guard let startDate = self.startDate else {
+            return
+        }
+        self.datePicker.date = startDate
         //TODO: Localize
         self.toolbarTitleLabel.text = "Set Start Time"
     }
     
     @IBAction func endViewTapped(sender: AnyObject) {
         self.isStartView = false
+        self.setStartEndViewSelected()
         self.showDatePickerView()
+        
+        guard let
+            startDate = self.startDate,
+            endDate = self.endDate else {
+                return
+        }
+        self.datePicker.minimumDate = self.dateByRoundingMinutesUpBy30(startDate)
+        self.datePicker.maximumDate = nil
+        self.datePicker.date = endDate
         //TODO: Localize
         self.toolbarTitleLabel.text = "Set End Time"
     }
     
+    private func setStartEndViewSelected() {
+        if (self.isStartView) {
+            //TODO: UIColor extension ,SpotHero blue
+            self.startDateLabel.textColor = UIColor(red: 0/255.0,
+                                                    green: 122/255.0,
+                                                    blue: 255/255.0,
+                                                    alpha: 1.0)
+            self.startTimeLabel.textColor = UIColor(red: 0/255.0,
+                                                    green: 122/255.0,
+                                                    blue: 255/255.0,
+                                                    alpha: 1.0)
+            self.endDateLabel.textColor = UIColor.blackColor()
+            self.endTimeLabel.textColor = UIColor.blackColor()
+        } else {
+            //TODO: UIColor extension ,SpotHero blue
+            self.endDateLabel.textColor = UIColor(red: 0/255.0,
+                                                  green: 122/255.0,
+                                                  blue: 255/255.0,
+                                                  alpha: 1.0)
+            self.endTimeLabel.textColor = UIColor(red: 0/255.0,
+                                                  green: 122/255.0,
+                                                  blue: 255/255.0,
+                                                  alpha: 1.0)
+            self.startDateLabel.textColor = UIColor.blackColor()
+            self.startTimeLabel.textColor = UIColor.blackColor()
+        }
+    }
+    
+    private func setStartEndViewUnselected() {
+        self.startDateLabel.textColor = UIColor.blackColor()
+        self.startTimeLabel.textColor = UIColor.blackColor()
+        self.endDateLabel.textColor = UIColor.blackColor()
+        self.endTimeLabel.textColor = UIColor.blackColor()
+    }
+    
+    
+    //MARK: Date Picker View - setup like collapsed search bar view?
+    
     private func setupDatePickerView() {
-        self.hideDatePickerView()
+        //TODO: Constant? -200
+        self.datePickerViewBottomConstraint.constant = -200
+        self.view.layoutIfNeeded()
         self.datePicker.minimumDate = self.dateByRoundingMinutesDownBy30()
         self.datePicker.date = self.dateByRoundingMinutesDownBy30()
     }
     
+    @IBAction func doneButtonPressed(sender: AnyObject) {
+        self.hideDatePickerView()
+        self.setStartEndViewUnselected()
+        self.showCollapsedSearchBar()
+    }
+    
+    @IBAction func datePickerValueDidChange(datePicker: UIDatePicker) {
+        self.datePickerDate = datePicker.date
+        guard let date = self.datePickerDate else {
+            return
+        }
+        self.setStartEndDateTimeLabelWithDate(date)
+    }
+    
+    private func setStartEndDateTimeLabelWithDate(date: NSDate) {
+        if (self.isStartView) {
+            self.startDate = date
+            if (self.endDate?.timeIntervalSinceDate(date) < 1800) {
+                self.endDate = self.dateByRoundingMinutesUpBy30(date)
+            }
+        } else {
+            self.endDate = date
+        }
+        
+        guard let
+            startDate = self.startDate,
+            endDate = self.endDate else {
+                return
+        }
+        self.startDateLabel.text = DateFormatter.PrettyMonthDayDate.stringFromDate(startDate)
+        self.startTimeLabel.text = DateFormatter.TimeOnly.stringFromDate(startDate)
+        self.endDateLabel.text = DateFormatter.PrettyMonthDayDate.stringFromDate(endDate)
+        self.endTimeLabel.text = DateFormatter.TimeOnly.stringFromDate(endDate)
+    }
+    
     private func showDatePickerView() {
-        UIView.animateWithDuration(0.3) { 
+        UIView.animateWithDuration(0.3) {
             self.datePickerViewBottomConstraint.constant = 0
             self.view.layoutIfNeeded()
         }
     }
     
     private func hideDatePickerView() {
-        UIView.animateWithDuration(0.3) { 
+        UIView.animateWithDuration(0.3) {
             self.datePickerViewBottomConstraint.constant = -200
             self.view.layoutIfNeeded()
         }
     }
     
-    @IBAction func doneButtonPressed(sender: AnyObject) {
-        self.hideDatePickerView()
-    }
-    
-    @IBAction func datePickerValueDidChange(datePicker: UIDatePicker) {
-        let date = datePicker.date
-        if (self.isStartView) {
-            //TODO: update start date time labels
-        } else {
-            //TODO: update end date time labels
-        }
-    }
-    
+    //TODO: Move to DateFormatter.swift, rename?
     private func dateByRoundingMinutesDownBy30() -> NSDate {
         let today = NSDate()
         let unitFlags: NSCalendarUnit = [.Minute, .Second]
@@ -170,6 +262,17 @@ class MapViewController: UIViewController {
         let interval: NSTimeInterval = Double(-((60 * remain) + timeComponents.second))
         return today.dateByAddingTimeInterval(interval)
     }
+    
+    //TODO: Move to DateFormatter.swift, rename?
+    private func dateByRoundingMinutesUpBy30(date: NSDate) -> NSDate {
+        let date = date
+        let unitFlags: NSCalendarUnit = [.Minute, .Second]
+        let timeComponents = NSCalendar.currentCalendar().components(unitFlags, fromDate: date)
+        let remain = timeComponents.minute % 30
+        let interval: NSTimeInterval = Double((60 * (30 - remain) - timeComponents.second))
+        return date.dateByAddingTimeInterval(interval)
+    }
+    
 }
 
 //MARK: PredictionControllerDelegate
@@ -187,7 +290,6 @@ extension MapViewController: PredictionControllerDelegate {
                 self.searchContainerViewHeightConstraint.constant = self.searchBarHeight + CGFloat(predictions.count) * rowHeight + headerFooterHeight * 2
                 self.reservationContainerViewHeightConstraint.constant = self.searchBarHeight + CGFloat(predictions.count) * rowHeight + headerFooterHeight * 2
             } else {
-                self.reservationContainerViewHeightConstraint.constant = self.reservationContainerViewHeight
                 self.searchContainerViewHeightConstraint.constant = self.searchBarHeight
             }
             self.view.layoutIfNeeded()
@@ -197,6 +299,7 @@ extension MapViewController: PredictionControllerDelegate {
     func didSelectPrediction(prediction: GooglePlacesPrediction) {
         self.searchBar.text = prediction.description
         self.showTimeSelectionView()
+        self.showCollapsedSearchBar()
     }
     
     func didTapXButton() {
