ef928d8d66b6e2a3ebdd2cf67822dd597c6833b0
Husein Kareem <huseinkareem@gmail.com>
2016-08-03 04:05:34 -0500

    setup date picker view

---
+++ SpotHero_iOS_Partner_SDK/Classes/DatePickerView.swift
@@ -0,0 +1,87 @@
+//
+//  DatePickerView.swift
+//  Pods
+//
+//  Created by Husein Kareem on 8/2/16.
+//
+//
+
+import UIKit
+
+protocol DatePickerViewDelegate {
+    func didPressDoneButton()
+    func didChangeDatePickerValue(date: NSDate)
+}
+
+protocol DatePickerDoneButtonDelegate {
+    func didPressDoneButton()
+}
+
+class DatePickerView: UIView {
+    @IBOutlet weak private var datePicker: UIDatePicker!
+    @IBOutlet weak private var toolbar: UIToolbar!
+    @IBOutlet weak private var datePickerViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak private var toolbarTitleLabel: UILabel!
+    
+    var doneButtonDelegate: DatePickerDoneButtonDelegate?
+    var delegate: DatePickerViewDelegate?
+    
+    private let DatePickerViewHeight: CGFloat = 200
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setupDatePickerView()
+    }
+    
+    private func setupDatePickerView() {
+        self.datePickerViewBottomConstraint.constant = -self.DatePickerViewHeight
+        self.datePicker.minimumDate = NSDate.dateByRoundingMinutesDownBy30(NSDate())
+        self.datePicker.date = NSDate.dateByRoundingMinutesDownBy30(NSDate())
+    }
+    
+    /**
+     Show or hide date picker view
+     
+     - parameter show: pass in true to show, false to hide
+     */
+    func showDatePickerView(show: Bool) {
+        UIView.animateWithDuration(0.3) {
+            self.datePickerViewBottomConstraint.constant = show ? 0 : -self.DatePickerViewHeight
+            self.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction private func doneButtonPressed(sender: AnyObject) {
+        self.showDatePickerView(false)
+        self.delegate?.didPressDoneButton()
+        self.doneButtonDelegate?.didPressDoneButton()
+    }
+    
+    @IBAction private func datePickerValueDidChange(datePicker: UIDatePicker) {
+        self.delegate?.didChangeDatePickerValue(datePicker.date)
+    }
+    
+}
+
+//MARK: TimeSelectionViewDelegate
+
+extension DatePickerView: TimeSelectionViewDelegate {
+    func didTapStartView(startDate: NSDate, endDate: NSDate) {
+        self.showDatePickerView(true)
+        self.datePicker.minimumDate = startDate
+        self.datePicker.date = startDate
+        self.toolbarTitleLabel.text = LocalizedStrings.SetStartTime
+    }
+    
+    func didTapEndView(startDate: NSDate, endDate: NSDate) {
+        self.showDatePickerView(true)
+        self.datePicker.minimumDate = NSDate.dateByRoundingMinutesUpBy30(startDate)
+        self.datePicker.maximumDate = nil
+        self.datePicker.date = endDate
+        self.toolbarTitleLabel.text = LocalizedStrings.SetEndTime
+    }
+    
+    func timeSelectionViewShouldHide() {
+        self.showDatePickerView(false)
+    }
+}
