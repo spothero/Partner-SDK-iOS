5248e8160ca0f371c481d78b1fc3b9b1821717ea
Husein Kareem <huseinkareem@gmail.com>
2016-08-03 15:46:57 -0500

    combines date round method into 1

--- SpotHero_iOS_Partner_SDK/Classes/Extensions/NSDate+DateFormatter.swift
+++ SpotHero_iOS_Partner_SDK/Classes/Extensions/NSDate+DateFormatter.swift
@@ -9,20 +9,21 @@
 import Foundation
 
 public extension NSDate {
-    static func dateByRoundingMinutesDownBy30(date: NSDate) -> NSDate {
+    /**
+     Round date time up/down by 30 mins
+     
+     - parameter roundDown: pass in true to round down by 30 mins, false to round up
+     */
+    func shp_dateByRoundingMinutesBy30(roundDown: Bool) -> NSDate {
         let unitFlags: NSCalendarUnit = [.Minute, .Second]
-        let timeComponents = NSCalendar.currentCalendar().components(unitFlags, fromDate: date)
+        let timeComponents = NSCalendar.currentCalendar().components(unitFlags, fromDate: self)
         let remain = timeComponents.minute % 30
-        let interval: NSTimeInterval = Double(-((60 * remain) + timeComponents.second))
-        return date.dateByAddingTimeInterval(interval)
-    }
-    
-    static func dateByRoundingMinutesUpBy30(date: NSDate) -> NSDate {
-        let date = date
-        let unitFlags: NSCalendarUnit = [.Minute, .Second]
-        let timeComponents = NSCalendar.currentCalendar().components(unitFlags, fromDate: date)
-        let remain = timeComponents.minute % 30
-        let interval: NSTimeInterval = Double((60 * (30 - remain) - timeComponents.second))
-        return date.dateByAddingTimeInterval(interval)
+        let interval: NSTimeInterval
+        if roundDown {
+            interval = Double(-((60 * remain) + timeComponents.second))
+        } else {
+            interval = Double((60 * (30 - remain) - timeComponents.second))
+        }
+        return self.dateByAddingTimeInterval(interval)
     }
 }
