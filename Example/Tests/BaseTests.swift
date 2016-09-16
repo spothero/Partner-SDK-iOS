e70cff1aa5506f2584d249b3c54c3cd26a9fa205
Matt Reed <matt.reed@spothero.com>
2016-09-16 11:13:39 -0500

    add random test email

--- Example/Tests/BaseTests.swift
+++ Example/Tests/BaseTests.swift
@@ -10,4 +10,9 @@ import XCTest
 
 class BaseTests: XCTestCase {
     let testEmail = "matt@gmail.com"
+    
+    var testEmailRandom: String {
+        let rand = arc4random_uniform(100000)
+        return "matt\(rand)@test.com"
+    }
 }
