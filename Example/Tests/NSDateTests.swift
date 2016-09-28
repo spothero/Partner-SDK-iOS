6f5e83d81edec3cbea682e0999807fbebfea56da
Matt Reed <matt.reed@spothero.com>
2016-09-28 16:11:04 -0500

    formatting

--- Example/Tests/NSDateTests.swift
+++ Example/Tests/NSDateTests.swift
@@ -43,42 +43,66 @@ class NSDateTests: XCTestCase {
         // 8:00pm
         let nonRounding = NSDate(timeIntervalSince1970: 1475110800)
         
-        self.checkTime(nonRounding, roundUpMinute: 30, roundDownMinute: 0, hour: 20)
+        self.checkTime(nonRounding,
+                       roundUpMinute: 30,
+                       roundDownMinute: 0,
+                       hour: 20)
         
         // 8:01pm
         let onePast = NSDate(timeIntervalSince1970: 1475110860)
         
-        self.checkTime(onePast, roundUpMinute: 30, roundDownMinute: 0, hour: 20)
+        self.checkTime(onePast,
+                       roundUpMinute: 30,
+                       roundDownMinute: 0,
+                       hour: 20)
         
         // 8:15pm
         let fifteenPast = NSDate(timeIntervalSince1970: 1475111700)
         
-        self.checkTime(fifteenPast, roundUpMinute: 30, roundDownMinute: 0, hour: 20)
+        self.checkTime(fifteenPast,
+                       roundUpMinute: 30,
+                       roundDownMinute: 0,
+                       hour: 20)
         
         // 8:29pm
         let OneMinuteBeforeThirty = NSDate(timeIntervalSince1970: 1475112540)
         
-        self.checkTime(OneMinuteBeforeThirty, roundUpMinute: 30, roundDownMinute: 0, hour: 20)
+        self.checkTime(OneMinuteBeforeThirty,
+                       roundUpMinute: 30,
+                       roundDownMinute: 0,
+                       hour: 20)
         
         // 8:30pm
         let nonRoundingThirty = NSDate(timeIntervalSince1970: 1475112600)
         
-        self.checkTime(nonRoundingThirty, roundUpMinute: 0, roundDownMinute: 30, hour: 21)
+        self.checkTime(nonRoundingThirty,
+                       roundUpMinute: 0,
+                       roundDownMinute: 30,
+                       hour: 21)
        
         // 8:31pm
         let onePastThirty = NSDate(timeIntervalSince1970: 1475112660)
         
-        self.checkTime(onePastThirty, roundUpMinute:  0, roundDownMinute: 30, hour: 21)
+        self.checkTime(onePastThirty,
+                       roundUpMinute: 0,
+                       roundDownMinute: 30,
+                       hour: 21)
         
         // 8:45pm
         let fifteenPastThirty = NSDate(timeIntervalSince1970: 1475113500)
 
-        self.checkTime(fifteenPastThirty, roundUpMinute:  0, roundDownMinute: 30, hour: 21)
+        self.checkTime(fifteenPastThirty,
+                       roundUpMinute: 0,
+                       roundDownMinute: 30,
+                       hour: 21)
         
         // 8:59pm
         let OneMinuteBefore = NSDate(timeIntervalSince1970: 1475114340)
         
-        self.checkTime(OneMinuteBefore, roundUpMinute:  0, roundDownMinute: 30, hour: 21)
+        self.checkTime(OneMinuteBefore,
+                       roundUpMinute: 0,
+                       roundDownMinute: 30,
+                       hour: 21)
     }
     
 }
