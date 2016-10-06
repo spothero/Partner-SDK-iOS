e5392d341c13908e2808a39a11b23998786ebf7f
Matt Reed <matt.reed@spothero.com>
2016-10-06 10:48:54 -0500

    Add docs

--- SpotHero_iOS_Partner_SDK/Classes/Extensions/UITextField+SpotHeroPartner.swift
+++ SpotHero_iOS_Partner_SDK/Classes/Extensions/UITextField+SpotHeroPartner.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 extension UITextField {
+    
+    /// Returns the position of the cursor. In an extension for easy reuse
+    ///
+    /// - parameter range:  Range of string
+    /// - parameter string: Replacement string for textfield
+    ///
+    /// - returns: Position of cursor
     func shp_getCursorPosition(range: NSRange, string: String) -> UITextPosition? {
         let beginning = self.beginningOfDocument
         return self.positionFromPosition(beginning, offset: (range.location + string.characters.count))
     }
     
+    /// Moves the cursor to the position passed in. In an extension for easy reuse
+    ///
+    /// - parameter cursorLocation: position to place cursor
     func shp_setCursorPosition(cursorLocation: UITextPosition?) {
         guard let cursorLocation = cursorLocation else {
             return
