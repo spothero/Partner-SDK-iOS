2845fa138ed3e9d24ec385edaa697cb03714fd49
Husein Kareem <huseinkareem@gmail.com>
2016-10-11 12:32:59 -0500

    structs to classes

--- SpotHero_iOS_Partner_SDK/Classes/Models/GooglePlacesPrediction.swift
+++ SpotHero_iOS_Partner_SDK/Classes/Models/GooglePlacesPrediction.swift
@@ -11,15 +11,14 @@ import Foundation
 /**
  *  Prediction of a google place
  */
-struct GooglePlacesPrediction {
-    let description: String
+@objc(SPHGooglePlacesPrediction)
+class GooglePlacesPrediction: NSObject {
+    let predictionDescription: String
     let placeID: String
     let terms: [String]
-}
-
-extension GooglePlacesPrediction {
+    
     init(json: JSONDictionary) throws {
-        self.description = try json.shp_string("description")
+        self.predictionDescription = try json.shp_string("description")
         self.placeID = try json.shp_string("place_id")
         let terms = try json.shp_array("terms") as [JSONDictionary]
         self.terms = try terms.map { term in
