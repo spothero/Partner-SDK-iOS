5e6e74b1f0e4aa2b6f54b73bed0bf8493de3532b
Husein Kareem <huseinkareem@gmail.com>
2016-11-09 14:22:01 -0600

    add example project

---
+++ SpotHero_Google_Places_Wrapper_Example/SpotHero_Google_Places_Wrapper_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotHero_Google_Places_Wrapper_Example
+//
+//  Created by Husein Kareem on 11/9/16.
+//  Copyright © 2016 SpotHero, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
